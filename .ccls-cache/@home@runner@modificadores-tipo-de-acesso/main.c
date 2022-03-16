0641842b79b7b8d0e79cdb8fb4d8ee795fb14863
Alex Vieira Lopes <86755313+zakale@users.noreply.github.com>
2022-03-16 23:38:24 +0000

    atualizado...

--- .ccls-cache/@home@runner@modificadores-tipo-de-acesso/main.c
+++ .ccls-cache/@home@runner@modificadores-tipo-de-acesso/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+void sp_to_dash(const char *str);
+void main(void) {
+  sp_to_dash("isso é um teste"); 
+}
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+void sp_to_dash(const char *str){
+  while(*str){
+    if(*str == ' ') printf("%c", '-');
+    else printf("%c", *str);
+    str++;
+  }
 }
